a8c6541ea23c3dd267d3869632aa1da952efe3e6
Cealrglario <kirstencarl.elgario@ucalgary.ca>
2024-03-11 14:39:34 -0600

    Step B

---
+++ src/components/ProductList.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../services/apiService';
+import { useProductsContext } from '../context/ProductsContext';
+
+const ProductList = () => {
+  const { products, setProducts } = useProductsContext();
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const data = await fetchProducts();
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+    getProducts();
+  }, [setProducts]);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.id}>{product.title}</div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
